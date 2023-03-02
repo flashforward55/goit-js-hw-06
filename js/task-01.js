c9f4112928ca89acc3ce070df9e949403deb8d82
Anton Styazhkin <rudana089@gmail.com>
2023-03-02 14:59:17 +0200

    Update task-02.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,9 +4,9 @@ const cotegoriesCounter = categoriesList.querySelectorAll('li.item').length;
 console.log(`Number of categories: ${cotegoriesCounter}`);
 
 categoriesList.querySelectorAll('li.item').forEach(category => {
-  const cotegoryHeading = category.querySelector('h2').textContent;
-  const elementCount = category.querySelectorAll('li').length;
+    const cotegoryHeading = category.querySelector('h2').textContent;
+    const elementCount = category.querySelectorAll('li').length;
 
-  console.log(`Category: ${cotegoryHeading}`);
-  console.log(`Elements: ${elementCount}`);
+    console.log(`Category: ${cotegoryHeading}`);
+    console.log(`Elements: ${elementCount}`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ul = document.querySelector('#ingredients');
+
+const liElements = ingredients.map(ingredient => {
+    const li = document.createElement('li');
+    li.textContent = ingredient;
+    li.classList.add('item');
+    return li;
+});
+
+ul.append(...liElements);
