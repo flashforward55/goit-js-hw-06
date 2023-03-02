1de560d6585757b19f1f6329b28b20eded0fde03
Anton Styazhkin <rudana089@gmail.com>
2023-03-02 23:03:17 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', e => {
+	e.preventDefault();
+	const {
+		elements: { email, password },
+	} = e.currentTarget;
+
+	if (email.value === '' || password.value === '') {
+		return alert('Please fill in all the fields!');
+	}
+
+	const formData = {
+		email: email.value,
+		password: password.value,
+	};
+
+	console.log(formData);
+	e.currentTarget.reset();
+});
