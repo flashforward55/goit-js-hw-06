04b018390e6181ab89c6af340086ebc9822fd97a
Anton Styazhkin <rudana089@gmail.com>
2023-03-03 20:23:58 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,40 @@
+const input = document.querySelector('#controls input');
+const createButton = document.querySelector('#controls [data-create]');
+const destroyButton = document.querySelector('#controls [data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+	let width = 30;
+	let height = 30;
+
+	for (let i = 0; i < amount; i += 1) {
+		const box = document.createElement('div');
+
+		box.style.width = `${width}px`;
+		box.style.height = `${height}px`;
+		box.style.backgroundColor = getRandomHexColor();
+
+		boxes.appendChild(box);
+
+		width += 10;
+		height += 10;
+	}
+}
+
+function destroyBoxes() {
+	boxes.innerHTML = '';
+}
+
+createButton.addEventListener('click', () => {
+	const amount = input.value;
+
+	createBoxes(amount);
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
